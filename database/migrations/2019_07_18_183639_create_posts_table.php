ca91fabaf2f3c0fb4efd88813b3ec5accd5bd4c9
vahid-md <vahid73.mohammadi@yahoo.com>
2019-07-24 00:05:46 +0430

    Add admin panel template

---
+++ database/migrations/2019_07_18_183639_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->nullable();
+            $table->text('body')->nullable();
+            $table->string('tag')->nullable();
+            $table->string('author')->nullable();
+            $table->integer('category_id')->nullable();
+            $table->string('photo_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
